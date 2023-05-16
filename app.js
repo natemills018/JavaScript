a274ed443e67b2d57a36d7757f9b2cf83cb12106
Nate Mills <natemills018@gmail.com>
2023-05-16 09:04:37 -0400

    JavaScript Kitchen Sink

---
+++ app.js
@@ -0,0 +1,12 @@
+let sum = add(2142, 9523)
+let val = subtract(add(12,43), 32)
+console.log(val);
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
